9f67a7249c71ea9988b1a5147e8974c79c07e9f5
Andres Revollo <andres.revollo@usb.edu.bo>
2024-02-26 18:19:46 -0400

    Funcionalidad de retornar Fizz cuando se ingresa 3

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,11 @@
 function generarFizzbuzz (n){
    
-    return  n + "";
+    //return n + "";
+    if (n % 3 === 0){
+        return "Fizz";
+    }
+    else{
+        return n + "";
+    }
 }
 export default generarFizzbuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -6,5 +6,8 @@ describe("Fizzbuzz", () => {
     });
 });
 it("debería generar el mismo número para otro numero que no tiene reglas como para el 2", () => {
-    expect(generarFizzbuzz(1)).toEqual("1");
+    expect(generarFizzbuzz(2)).toEqual("2");
+});
+it("debería generar el fizz para un numero que sigue esa regla como para el 3", () => {
+    expect(generarFizzbuzz(3)).toEqual("Fizz");
 });
